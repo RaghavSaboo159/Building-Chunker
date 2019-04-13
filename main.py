644a8147f25613772a54a4a37fade4e8f1a9c407
Jaidev <jaidevtrumpet@gmail.com>
2019-04-13 17:02:58 +0530

    Ready for Flask

--- main.py
+++ main.py
@@ -4,11 +4,26 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 
 app = Flask(__name__)
 
+# database for the forum posts, the experiment it is used for 
 db = create_engine("sqlite:///app.db")
+
+# Standard Routes for all web pages, the function names are used in the html for all anchor links
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 	return render_template("index.html");
+
+@app.route("/Experiment", methods=["GET", "POST"])
+def experiments():
+	return render_template("experiments.html");
+
+@app.route("/Contribute", methods=["GET", "POST"])
+def contribute():
+	return render_template("contribute.html");
+
+@app.route("/Building-Chunker", methods=["GET", "POST"])
+def buildingchunker():
+	return render_template("Experiments/Building-Chunker/index.html")
 
 if __name__ == '__main__':
 	app.run()
