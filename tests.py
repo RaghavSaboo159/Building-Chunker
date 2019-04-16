345d1f1c33531d14d8ea615fec8d733cbde914c7
Jaidev <jaidevtrumpet@gmail.com>
2019-04-16 19:51:29 +0530

    Removing Print from tests.py

--- tests.py
+++ tests.py
@@ -8,7 +8,6 @@ class FlaskTestCase(unittest.TestCase):
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        print(response)
         self.assertEqual(response.status_code, 200)
 
     def test_experiment(self):
